99654141d0ad0a5a9974df4985e98787d1da99d3
Claudio Puggioni <claudieddu@hotmail.com>
2022-09-04 22:23:37 -0700

    finished

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef, useState } from "react";
+import Modal from "./Modal";
+import "./styles.css";
+
+export default function UserInterface() {
+  const [images, setImages] = useState(false);
+  const searchRef = useRef(null);
+
+  async function searchImg(query) {
+    let API_URL = `https://api.unsplash.com/search/photos?client_id=56p71PWCNpxHLfx1glbJg94czzJKzQfmDmoykqYcYic&query=${query}&per_page=30"`;
+    let response = await fetch(API_URL);
+    let data = await response.json();
+    console.log(data);
+    setImages(data.results);
+  }
+
+  useEffect(() => {
+    searchImg("random");
+  }, []);
+
+  return (
+    <div id="container">
+      <div id="appHeader">Image Search Engine</div>
+      <div id="searchDiv">
+        <input
+          ref={searchRef}
+          id="searchInput"
+          name="searchInput"
+          type="text"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchImg(searchRef.current.value);
+              searchRef.current.value = "";
+            }
+          }}
+        />
+        <button
+          id="searchBtn"
+          onClick={() => {
+            searchImg(searchRef.current.value);
+            searchRef.current.value = "";
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div id="imagesDiv">
+        {images ? (
+          images.map((ele) => <Modal imgInfo={ele} />)
+        ) : (
+          <>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+            <div className="tempImage"></div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/Modal.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+
+export default function Modal({ imgInfo }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <img onClick={handleOpen} src={imgInfo.urls.thumb} alt={`${imgInfo.alt_description}-thumb`} />
+      <div
+        className="modalBox"
+        onClick={handleClose}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          display: !open ? "none" : "flex",
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "rgba(0, 0, 0, 0.866)",
+        }}
+      >
+        <img
+          src={imgInfo.urls.full}
+          alt={`${imgInfo.alt_description}-full`}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        />
+      </div>
+    </>
+  );
+}
